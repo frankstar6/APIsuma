0fc82a5103c4728fe332603e0b4f928acf81cb82
franciscoestrella631@gmail.com <franciscoestrella631@gmail.com>
2025-03-21 09:45:58 -0600

    ac

--- src/index.js
+++ src/index.js
@@ -1,89 +1,10 @@
-// //const morgan = require("morgan");
-
-// const express = require(exports);
-// const app = express();
-// const morgan = require(morgan);
-
-// //configuracion del servidor en el puerto 3000
-// app.set('port', process.env.PORT || 3000);
-// app.set('json spaces', 2);
-
-// //MiddleWare
-// //investigar que es MiddleWare para mostar resultados o para jeuctar otro proceso que comunica dos funciones
-// //funcion que recibe el resultado de la funcion para que los procesos reciban esa funcion
-// //la salida de la funcion que esta el medio de funcion middle ware funcion 3 la del medio "funcion middle ware" para que funcionen los 1 y 3
-// //para ejecutar todas las peticiones de express para que tengan la salidad json los mensajes los saca de salid json morgan lo que saque
-// app.use(morgan('dev'));
-// app.use(express.urlencodd({extended:false}));
-// app.use(express.json()); 
-
-// //nuestro primer web service '/' el directorio raiz donde lo va a levantar cuando se escribe
-// app.get('/', (req, res)=>{
-//     res.json(
-//         {
-//             "mensaje": "esta es mi primer api"
-//         }
-//     );
-// });
-
-// //iniciando el servidor
-// app.listen(app.get('port'), ()=>{
-//     console.log('Servidor localhost en puerto 3000');
-// });
-// Importación de módulos
-// const express = require("express");
-// const morgan = require("morgan");
-
-// const app = express();
-
-// // Configuración del servidor
-// app.set("port", process.env.PORT || 3000);
-// app.set("json spaces", 2);
-
-// // Middleware
-// app.use(morgan("dev"));
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
-// // Ruta principal
-// app.get("/", (req, res) => {
-//   res.json({
-//     mensaje: "Esta es mi primera API",
-//   });
-// });
-
-// // Iniciando el servidor
-// app.listen(app.get("port"), () => {
-//   console.log(`Servidor corriendo en el puerto ${app.get("port")}`);
-// });
-
-// //enpoint ara sumar dos numeros
-// app.post('/sumar', (req, res)=>{
-//   const { num1, num2 } = req.body;
-
-//   //validar que se hayan los dos numeros que no esten vacio
-//   if(!num1 || num2){
-//     return res.status(400).send({error: 'Faltan numeros para sumar'});
-//   }
-
-//   //sumar los numeros
-//   const resultado = num1 + num2;
-
-//   //enviar el resultado
-//   res.send({resultado})
-// });
-
-// //inciando el servidor
-// app.listen(app.set('port'), () =>{
-//   console.log(`Servidor corriendo en el puerto ${app.get("port")}`)
-// });
 
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
-
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 app.set('json spaces', 2); 
 
 // Middleware
@@ -94,7 +15,7 @@ app.use(express.json());
 // Nuestro primer web service
 app.get('/', (req, res) => {
     res.json({
-        "mensaje": "Hola soy Hernandez Camero Atlai"
+        "mensaje": "mi primer api"
     }
   );
 });
@@ -115,23 +36,23 @@ app.post('/sumar', (req, res) => { //http://localhost/sumar
 });
 
 //programar un entepoint que calcule el area de un triangulo
-app.post('/area', (req, res) => {
-  const {base , altura} = req.body;
-  if (base == undefined || altura == undefined){
-    return res.status(400).send({ error: "Faltan de agregar numeros, proporcione base y altura"});
-  }
+//app.post('/area', (req, res) => {
+  //const {base , altura} = req.body;
+  //if (base == undefined || altura == undefined){
+    //return res.status(400).send({ error: "Faltan de agregar numeros, proporcione base y altura"});
+  //}
 
-  if(isNaN(base) || isNaN(altura)){
-    return res.status(400).send({ error: "Base y Altura tienen que ser numeros"});
-  }
+  //if(isNaN(base) || isNaN(altura)){
+    //return res.status(400).send({ error: "Base y Altura tienen que ser numeros"});
+  //}
   //operacion
-  const medio = 0.5;
-  const operacion = medio * Number(base) * Number(altura);
+  //const medio = 0.5;
+  //const operacion = medio * Number(base) * Number(altura);
   //enviar operacion
-  res.json({area: operacion})
-}); 
+  //res.json({area: operacion})
+//}); 
 
 // Iniciando el servidor
-app.listen(app.get('port'), () => {
-  console.log(`Servidor corriendo en el puerto ${app.get("port")}`)
-});
+//app.listen(app.get('port'), () => {
+  //console.log(`Servidor corriendo en el puerto ${app.get("port")}`)
+//});
